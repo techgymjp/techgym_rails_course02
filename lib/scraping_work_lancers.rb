9a066be800f4ed33ece1527f1686b2452a468814
techgym <tadashifujinaga@gmail.com>
2024-06-27 21:20:28 +0900

    Update scraping_work_lancers.rb

--- lib/scraping_work_lancers.rb
+++ lib/scraping_work_lancers.rb
@@ -26,7 +26,7 @@ module ScrapingWorkLancers
   end
 
   def self.title(doc)
-    doc.at('//section[contains(@class, "section-title-group")]/h1[contains(@class, "heading--lv1")]/text()').text.strip
+    doc.at('//h1[contains(@class, "c-heading--lv1")]').text.strip
   end
 
   def self.site_type
