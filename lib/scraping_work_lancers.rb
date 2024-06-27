e09375363a8dbc185135039073f86f72f7e79dec
techgym <tadashifujinaga@gmail.com>
2024-06-27 21:14:57 +0900

    Update scraping_work_lancers.rb

--- lib/scraping_work_lancers.rb
+++ lib/scraping_work_lancers.rb
@@ -23,7 +23,7 @@ class ScrapingWorkLancers < ScrapingWork
   end
 
   def self.detail(doc)
-    details = doc.search('//section[contains(@class, "section-work-detail-content")]/dl[contains(@class, "c-definitionList")]').map { |dl| dl.text.strip }
+    details = doc.search('//dl[contains(@class, "c-definition-list")]/dd[contains(@class, "c-definition-list__description")]').map { |dd| dd.text.strip }
     details.join
   end
 
